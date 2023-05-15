03411907c16b47b681da65b6d213b84a6216af95
Lara Boarro <larasouzab@gmail.com>
2023-05-15 14:04:57 -0300

    fix

--- resources/views/front/themes/theme-15/components/header.blade.php
+++ resources/views/front/themes/theme-15/components/header.blade.php
@@ -155,8 +155,8 @@
                                             <div>{{ $category->name }}</div>
                                             <div class="boxsubcat">
                                                 <div class="d-flex">
-                                                    @foreach  ($category->products as $product)
-                                                        {{-- @if ($product->show_in_navbar)
+                                                    @foreach ($category->products as $product)
+                                                        @if ($product->show_in_navbar)
                                                             <a href="{{ route('front.product', $product->slug ) }}" class="py-1 d-block">
                                                                 <div class="box-img">
                                                                     <img src="{{ asset('storage/images/thumbnails/' . $product->thumbnail) }}" class="img-fluid" alt="">
@@ -164,7 +164,7 @@
                                                                 <p class="brand-name">{{str($product->brand->name)->limit(8,'...')}}</p>
                                                                 <p class="product-name">{{str($product->name)->limit(25,'...')}}</p>
                                                             </a>
-                                                        @endif --}}
+                                                        @endif
                                                     @endforeach
                                                 </div>
                                                 <a class="link-seemore" href="{{ route('front.category', $category->slug )}}"> Ver todos {{ $category->name }}</a>
