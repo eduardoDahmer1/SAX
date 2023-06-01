c557f328f1babf8ecb31c0834019870d5befcfcc
Allan <alan@crowtech.digital>
2023-06-01 15:40:44 -0300

    removing protect observer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -22,11 +22,7 @@ class EventServiceProvider extends ServiceProvider
      * The event listener mappings for the application.
      *
      * @var array
-     */
-    protected $observers = [
-        Order::class => [OrderObserver::class],
-    ];
-    
+     */    
 
     protected $listen = [
         'App\Events\Event' => [
