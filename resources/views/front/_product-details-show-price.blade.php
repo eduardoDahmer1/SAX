51c083e846f9671bf7eae68d5ba5eb18ffab3190
José Olavo <joseolavopossolli04@gmail.com>
2023-07-31 14:22:09 -0300

    adjust-in-product-page

--- resources/views/front/_product-details-show-price.blade.php
+++ resources/views/front/_product-details-show-price.blade.php
@@ -9,17 +9,18 @@
     }
     @endphp
     @if(!config("features.marketplace"))
-    <p class="price"><span id="sizeprice">{{ $highlight }}</span>
+    <p class="price">
         @php
         $size_price_value = $productt->vendorPrice() * $product_curr->value;
         $previous_price_value = $productt->previous_price * $product_curr->value *
         (1+($gs->product_percent / 100));
         @endphp
+        @if($productt->previous_price)
         <small>
-            <del id="previousprice" style="display:{{($size_price_value >= $previous_price_value)? 'none' : '' }};">
-                {{$productt->showPreviousPrice() }}
-            </del>
+            <span style="font-weight: 400; text-decoration: line-through; color: #bababa;">{{$curr->sign}}{{$productt->previous_price}}</span>
         </small>
+        @endif
+        <span id="sizeprice">{{ $highlight }}</span>
         <input type="hidden" id="previous_price_value" value="{{ round($previous_price_value,2) }}">
         @if($curr->id != $scurrency->id)
         <small><span id="originalprice">{{ $small }}</span></small>
