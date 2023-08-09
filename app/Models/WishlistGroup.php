f610daaa4000b4bf788e8b142b5f292edc43a2bd
Antonio850 <antonio@crowtech.digital>
2023-08-09 09:28:47 -0300

    feature/model: wishlist group

---
+++ app/Models/WishlistGroup.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WishlistGroup extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Get the wishlist
+     */
+    public function wishlists()
+    {
+        return $this->hasMany(Wishlist::class);
+    }
+
+    /**
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function scopeCurrentUser($query, User $user)
+    {
+        $query->where('user_id', $user->id);
+    }
+}
