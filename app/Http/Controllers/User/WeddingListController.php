18efe8bdc3ac4a9b2e41df0b17083c22ef58a960
Antonio850 <antonio@crowtech.digital>
2023-08-23 16:35:52 -0300

    feature/wedding/controller: associate product with user

--- app/Http/Controllers/User/WeddingListController.php
+++ app/Http/Controllers/User/WeddingListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class WeddingListController extends Controller
 {
@@ -15,6 +16,20 @@ class WeddingListController extends Controller
     {
         return view('user.wedding.index', [
             'products' => auth()->user()->weddingProducts,
+        ]);
+    }
+
+    public function store(Request $request, $product)
+    {
+        $message = __("Successfully Product Added To Wedding List");
+        if ($request->user()->weddingProducts()->where('product_id', $product)->count()) {
+            $message = __("Successfully Product Removed From Wedding List");
+        }
+
+        $request->user()->weddingProducts()->toggle($product);
+
+        return response()->json([
+            "success" => $message,
         ]);
     }
 }
