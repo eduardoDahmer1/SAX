e168b77ee88f040573272245733520d165004c87
Antonio850 <antonio@crowtech.digital>
2023-08-24 09:32:34 -0300

    feature/component: add to wedding list icon

---
+++ resources/views/components/wedding/product-add-icon.blade.php
@@ -0,0 +1,20 @@
+@props(['id'])
+
+<li>
+    @wedding
+        @if (Auth::guard('web')->check())
+            <span class="add-to-wedding"
+                data-href="{{ route('user.wedding.store', $id) }}"
+                data-toggle="tooltip" data-placement="right"
+                title="{{ __('Add To Wedding List') }}" data-placement="right">
+                <i class="fas fa-gift" style="font-size: 1.3rem"></i>
+            </span>
+        @else
+            <span rel-toggle="tooltip" title="{{ __('Add To Wedding List') }}"
+                data-toggle="modal" id="wish-btn" data-target="#comment-log-reg"
+                data-placement="right">
+                <i class="fas fa-gift" style="font-size: 1.3rem"></i>
+            </span>
+        @endif
+    @endwedding
+</li>
