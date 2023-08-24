5e7b0bb7140edc9e237ffa05a733ed4b3f9bdf5b
Antonio850 <antonio@crowtech.digital>
2023-08-24 14:48:59 -0300

    enhancement/wedding/view/show: share options

--- resources/views/front/wedding/show.blade.php
+++ resources/views/front/wedding/show.blade.php
@@ -3,10 +3,36 @@
 <div class="container-fluid mt-2 mb-5">
     <div class="row px-5">
         <div class="col-12">
-            <h2 class="h1 mb-3">
-                {{__('Wedding List')}} {{__('of')}} {{$owner->name}}
-            </h2>
-            <div class="row border">
+            <div class="row w-100 justify-content-between mx-1">
+                <div>
+                    <h2 class="h1 mb-3">
+                        {{__('Wedding List')}} {{__('of')}} {{$owner->name}}
+                    </h2>
+                    <p class="text-muted mb-1">{{__('Email')}}: {{$owner->email}}</p>
+                    @if ($owner->phone)
+                        <p class="text-muted">{{__('Phone')}}: {{$owner->phone}}</p>
+                    @endif
+                </div>
+                @auth
+                    @if ($owner->id === auth()->user()->id)
+                        <div>
+                            <div class="d-flex">
+                                @if ($owner->is_wedding)    
+                                    <div id="share" class="mr-3 cursor-pointer" onclick="share()" style="margin-top: 4px;">
+                                        <i class="fas fa-share-alt"></i>
+                                        {{__('Share')}}
+                                    </div>
+                                @endif
+                                <div>
+                                    <input @checked($owner->is_wedding) class="styled-checkbox" id="checkboxPrivacy" type="checkbox" name="privacy">
+                                    <label for="checkboxPrivacy">{{ __('Public Wedding List') }}?</label>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endauth
+            </div>
+            <div class="row border mx-1">
                 @forelse ($products as $product)
                     <div class="col-12 py-3 px-2 aling-center border-top">
                         <div class="row">
@@ -27,6 +53,10 @@
                                                 <a class="btn btn-dark w-auto" href="{{route('user.wedding.buy', [$owner->id, $product->id])}}">
                                                     {{__('Shop Now')}}
                                                 </a>
+                                            @else
+                                                <x-wedding.product-add-icon 
+                                                    class="btn w-auto deleteProdWedd cursor-pointer" 
+                                                    icon="trash" :id="$product->id" title="Delete" />
                                             @endif
                                         @else
                                             <button class="btn btn-dark w-auto" rel-toggle="tooltip"
@@ -55,5 +85,22 @@
         let url = "{{ url()->current() }}"
         navigator.clipboard.writeText(url)
     }
+
+    $('.deleteProdWedd').on('click', function(ele) {
+        $( this ).parent().parent().parent().parent().remove();
+    });
+
+    $("#checkboxPrivacy").on('click', function() {
+        var url = "{{route('user.wedding.privacy')}}"
+        
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            type: 'POST',
+            url: url,
+            success: () => window.location.reload()
+        });
+    });
 </script>
 @endsection
