22502528a5b0ce054ebac63c9c95d5cef39ba2c7
José Olavo <joseolavopossolli04@gmail.com>
2023-11-08 13:04:56 -0300

    fix-columns-in-coupons-table

--- database/migrations/2023_10_20_112333_add_category_and_brand_columns_to_coupons.php
+++ database/migrations/2023_10_20_112333_add_category_and_brand_columns_to_coupons.php
@@ -6,27 +6,4 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('coupons', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id')->nullable();
-            $table->unsignedBigInteger('brand_id')->nullable();
-    
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
-            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('set null');
-        });
-    }
-    
-    public function down()
-    {
-        Schema::table('coupons', function (Blueprint $table) {
-            $table->dropColumn('category_id');
-            $table->dropColumn('brand_id');
-        });
-    }
 };
