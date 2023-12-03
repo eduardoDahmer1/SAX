e33c47954984b8f20d37ce7695da7ac0f20d84e1
Matteo Carminato <mcarminato95@gmail.com>
2023-12-03 09:02:29 -0300

    remove themes

--- resources/views/front/themes/theme-01/layout.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('front.themes.shared.layout')
-
-@section('before-footer')
-@include('front.themes.theme-01.components.services')
-@endsection
--- resources/views/front/themes/theme-02/layout.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('front.themes.shared.layout')
-
-@section('styles')
-<link rel="stylesheet" href="{{asset('assets/front/themes/theme-02/assets/css/categorie_horizontal.css')}}">
-
-@endsection
