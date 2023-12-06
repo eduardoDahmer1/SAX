4385470aaba82ca20600d9d26702dff8d26a8119
Anderson <anderson@crowtech.digital>
2023-12-06 15:58:37 -0300

    Creating all notifications asynchronously and triggering the function when scrolling the scroll

--- resources/views/admin/notification/popup.blade.php
+++ resources/views/admin/notification/popup.blade.php
@@ -92,3 +92,118 @@
 </a>
 
 @endif
+<script>
+    var locale = "{{$lang->locale}}"
+    moment.locale(locale)
+        $(document).ready(function() {
+         function loadNotifications(){
+              return $.ajax({
+                    url: '/admin/notifications/notification',
+                    method: 'GET',
+                    dataType: "json",
+                    success: function(data) {
+                        var notifications = data.data;
+                        var dropdownMenu = $('.dropdownmenu-wrapper');
+                        var html = '<ul>'; 
+                        $.each(notifications, function(i, notification) {
+                            var createdAt = moment(notification.created_at);
+                            var timeAgo = createdAt.fromNow()
+                            if(notification.order_id && !notification.receipt){
+                                html += '<li style="opacity:' + (notification.is_read ? '50%' : '') + '">';
+                                html += `<a href="/admin/order/${notification.order_id}/show">`;
+                                html += '<i class="fas fa-2x fa-newspaper"></i>';
+                                html += " {{ __('New Order') }}: " + notification.order.order_number; 
+                                html += '<small class="d-block notf-time">' + timeAgo + '</small>';
+                                html += '</a>';
+                                html += `<a class="clear-notf" style="float: right;
+                                position: relative;
+                                display: flex;
+                                color: black !important;
+                                padding: 0.5rem !important;" href="/admin/notifications/clear/${notification.id}">Limpar</a>`;
+                                html += '</li>';
+                            }
+                            else if(notification.user_id){
+                                html += '<li style="opacity:' + (notification.is_read ? '50%' : '') + '">';
+                                html += `<a href="/admin/user/${notification.user_id}/show">`;
+                                html += '<i class="fas fa-user"></i>';
+                                html += " {{ __('New User') }}: " + notification.user.name; 
+                                html += '<small class="d-block notf-time">' + timeAgo + '</small>';
+                                html += '</a>';
+                                html += `<a class="clear-notf" style="float: right;
+                                position: relative;
+                                display: flex;
+                                color: black !important;
+                                padding: 0.5rem !important;" href="/admin/notifications/clear/${notification.id}">Limpar</a>`;
+                                html += '</li>';
+                            }
+                            else if(notification.product_id){
+                                html += '<li style="opacity:' + (notification.is_read ? '50%' : '') + '">';
+                                html += `<a href="/admin/products/edit/${notification.product_id}">`; 
+                                html += '<i class="icofont-cart"></i>{{ __("Low Stock Product") }}';
+                                var productName = notification.product.name;
+                                if(productName.length > 50) {
+                                    productName = productName.substring(0, 30) + "...";
+                                }
+                                html += '<small class="d-block notf-stock">' + productName + '</small>';
+                                html += '<small class="d-block notf-stock">' + "{{ __('Stock') }}: " + notification.product.stock +'</small>';
+                                html += '<small class="d-block notf-time">' + timeAgo + '</small>';  
+                                html += '</a>';
+                                html += `<a class="clear-notf" style="float: right;
+                                position: relative;
+                                display: flex;
+                                color: black !important;
+                                padding: 0.5rem !important;" href="/admin/notifications/clear/${notification.id}">Limpar</a>`;
+                                html += '</li>';
+
+                            }else if(notification.conversation_id){
+                                html += '<li style="opacity:' + (notification.is_read ? '50%' : '') + '">';
+                                html += `<a href="/admin/message/${notification.conversation_id}">`;
+                                html += '<i class="fas fa-envelope"></i>';
+                                html += "{{ __('New message from') }}: " + notification.conversation.user.name;
+                                html += '<small class="d-block notf-stock">' + notification.conversation.message + '</small>';
+                                html += '<small class="d-block notf-time">' + timeAgo + '</small>';
+                                html += '</a>';
+                                html += `<a class="clear-notf" style="float: right;
+                                position: relative;
+                                display: flex;
+                                color: black !important;
+                                padding: 0.5rem !important;" href="/admin/notifications/clear/${notification.id}">Limpar</a>`;
+                                html += '</li>';
+                                    
+                            }else if(notification.order_id && notification.receipt){
+                                html += '<li style="opacity:' + (notification.is_read ? '50%' : '') + '">';
+                                html += `<a href="/order/${notification.order_id}/receipt">`;
+                                html += '<i class="fas fa-2x fa-newspaper"></i>';
+                                html += "{{ __('A new receipt has been sent') }}";
+                                html += '<small class="d-block notf-time">' + timeAgo + '</small>';
+                                html += '</a>';
+                                html += `<a class="clear-notf" style="float: right;
+                                position: relative;
+                                display: flex;
+                                color: black !important;
+                                padding: 0.5rem !important;" href="/admin/notifications/clear/${notification.id}">Limpar</a>`;
+                                html += '</li>';
+                            }
+                        });
+                        html += '</ul>';
+
+                        dropdownMenu.append(html);
+                    }
+                });
+            }
+            var isLoading = false;
+            $('.dropdown-menu').on('scroll', function() {
+                var scrollTop = $(this).scrollTop();
+                var innerHeight = $(this).innerHeight();
+                var scrollHeight = $(this)[0].scrollHeight;
+                if(scrollTop + innerHeight >= scrollHeight -5 && !isLoading) {
+                    isLoading = true;
+                    loadNotifications().then(function() {
+                        isLoading = false;
+                    });
+                }
+            });
+
+        })
+
+</script>
