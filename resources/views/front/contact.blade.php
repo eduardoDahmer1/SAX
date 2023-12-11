0de196fbd39e6adcce78f9799eabd22c7e258817
danielcrowtech <daniel@crowtech.digital>
2023-12-11 10:16:28 -0300

    Centered Card content

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -109,8 +109,8 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4.222 19.778a4.983 4.983 0 0 0 3.535 1.462 4.986 4.986 0 0 0 3.536-1.462l2.828-2.829-1.414-1.414-2.828 2.829a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.829-2.828-1.414-1.414-2.829 2.828a5.006 5.006 0 0 0 0 7.071zm15.556-8.485a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0L9.879 7.051l1.414 1.414 2.828-2.829a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.829 2.828 1.414 1.414 2.829-2.828z"></path><path d="m8.464 16.95-1.415-1.414 8.487-8.486 1.414 1.415z"></path></svg>
                                     </div>
                                 </div>
-                                <div class="content d-flex align-self-center">
-                                    <div class="content">
+                                <div class="content d-flex align-self-center text-center">
+                                    <div class="">
                                         @if ($ps->site != null && $ps->email != null)
                                             <a href="{{ $ps->site }}" target="_blank">{{ $ps->site }}</a>
                                             <a href="mailto:{{ $ps->email }}">{{ $ps->email }}</a>
@@ -130,8 +130,8 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M12 2C7.589 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.029-5.56 8-12 0-4.411-3.589-8-8-8zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z"></path></svg>
                                     </div>
                                 </div>
-                                <div class="content d-flex align-self-center">
-                                    <div class="content">
+                                <div class="content d-flex align-self-center text-center">
+                                    <div class="">
                                         @if ($ps->street != null)
                                             {!! $ps->street !!}
                                         @endif
@@ -146,8 +146,8 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg>
                                 </div>
                             </div>
-                            <div class="content d-flex align-self-center">
-                                <div class="content">
+                            <div class="content d-flex align-self-center text-center">
+                                <div class="">
                                     @if ($ps->phone != null && $ps->fax != null)
                                         <a href="tel:{{ $ps->phone }}">{{ $ps->phone }}</a>
                                         <a href="tel:{{ $ps->fax }}">{{ $ps->fax }}</a>
