8c3f77f4538e1662e005a8b4413d98ddff32fcdc
José Olavo <joseolavopossolli04@gmail.com>
2023-12-15 11:42:56 -0300

    replace-route

--- resources/views/front/checkout.blade.php
+++ resources/views/front/checkout.blade.php
@@ -2261,7 +2261,7 @@
     $(document).ready(function () {
         $.ajax({
             type: 'GET',
-            url: 'https://localhost' + '/checkout/getStatesOptions',
+            url: 'https://shop.saxdepartment.com' + '/checkout/getStatesOptions',
             data: {
                 location_id: 173 //paraguai
             },
@@ -2283,7 +2283,7 @@
             var selectedValue = $(this).val();
             $.ajax({
                 type: 'GET',
-                url: 'https://localhost' + '/checkout/getCitiesOptions',
+                url: 'https://shop.saxdepartment.com' + '/checkout/getCitiesOptions',
                 data: {
                     location_id: selectedValue //paraguai
                 },
