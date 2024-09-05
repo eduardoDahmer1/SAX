ac86cc24bec849810be4a617eee1f7960a2fcf9b
EduardoDahmer1 <eduluizdahmer@gmail.com>
2024-09-05 11:34:47 -0300

    fix/login-status

--- resources/views/front/themes/shared/components/modal-simplified-checkout.blade.php
+++ resources/views/front/themes/shared/components/modal-simplified-checkout.blade.php
@@ -3,14 +3,14 @@
     aria-labelledby="simplified-checkout-modal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header top-header" style="padding: 1rem 1rem !important; border-radius: 0 !important;">
-                <div class="modal-title" style="color: #fff">{{ __('Simplified Checkout') }}</div>
+            <div class="modal-header top-header algin-simplify-checkout" style="padding: 1rem 1rem !important; border-radius: 0 !important;">
+                <div class="modal-title"><h3>{{ __('Simplified Checkout') }}</h3></div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="container mt-5">
-                <p>{{ __('Buy in a simple and interactive way') }}</p>
+            <div class="container algin-simplify-checkout">
+                <h5>{{ __('Buy in a simple and interactive way') }}</h5>
             </div>
             <div class="modal-body mt-3">
                 @include('includes.simplified-checkout')
