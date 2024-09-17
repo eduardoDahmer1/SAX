800bd2b3eaa79b4bbf18cc194316e53807f28498
EduardoDahmer1 <eduluizdahmer@gmail.com>
2024-09-17 16:29:20 -0300

    feature/banner-width-bridal

--- resources/views/front/themes/theme-15/components/large-banners.blade.php
+++ resources/views/front/themes/theme-15/components/large-banners.blade.php
@@ -1,25 +1,19 @@
 @if($ps->large_banner == 1)
 <!-- Banner Area One Start -->
 <section class="banner-section" data-aos="fade-in" data-aos-delay="100">
-    <div class="container">
-        <div class="row">
-            @foreach($large_banners->chunk(1) as $chunk)
-            @foreach($chunk as $img)
-            <div class="col-lg-12">
-                <div class="img">
-                    <a class="{{ env('THEME') == " theme-15" ? "" : "banner-effect" }} banner-w100"
-                        href="{{ $img->link }}">
-                        <figure>
-                            <img src="{{asset('storage/images/banners/'.$img->photo)}}" alt="Banner">
-                        </figure>
-                    </a>
-                </div>
-            </div>
-            @endforeach
-            @break
-            @endforeach
+    @foreach($large_banners->chunk(1) as $chunk)
+    @foreach($chunk as $img)
+        <div class="img">
+            <a class="{{ env('THEME') == " theme-15" ? "" : "banner-effect" }} banner-w100"
+                href="{{ $img->link }}">
+                <figure>
+                    <img src="{{asset('storage/images/banners/'.$img->photo)}}" alt="Banner">
+                </figure>
+            </a>
         </div>
-    </div>
+    @endforeach
+    @break
+    @endforeach
 </section>
 <!-- Banner Area One Start -->
 @endif
